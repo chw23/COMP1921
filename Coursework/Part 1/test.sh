693ec7326cef492e7dd40c6594205cd55391b2de
Tommy Cheuk Ho Wong <146346053+chw23@users.noreply.github.com>
2024-02-13 20:25:52 +0000

    Test plan finished

---
+++ Coursework/Part 1/test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo -e "\n~~~~~~~ TEST START ~~~~~~~" 
+
+echo -e "\n\n~~ FILE INPUT ~~"
+
+# testing for not passing a file into the programme
+echo -n "Testing no file - "
+
+# testing for wrong file name
+echo -n "Testing file not found - "
+
+# SUCCESSFUL
+
+echo -n "Testing correct file - "
+
+echo -e "\n\n ~~ FILE FORMAT ~~"
+
+
+
+
+
+
+
+
+
+
+
+rm -f tmp
