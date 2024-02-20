69e20ce900272fcb009f7e6ae6f1923c3e461510
Tommy Cheuk Ho Wong <146346053+chw23@users.noreply.github.com>
2024-02-20 13:49:11 +0000

    skeleton 10%

--- Coursework/Part_1/maze.c
+++ Coursework/Part_1/maze.c
@@ -1,6 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main () {
+void OpenMaze (char filename) {
+    
+}
+
+
+
+int main (int argc, char* argv[]) {
+
+    // users pass the maze file through commandline
+
+    OpenMaze (filename);
+
+
+    // Start playing the maze
+
+    switch (input) {
+        case "A":
+        case "a":
+
+            // Going to the left
+
+            break;
+
+        case "S":
+        case "s":
+
+            // Going down
+
+            break;
+
+        case "D":
+        case "d":
+
+            // Going to the right
+
+            break;
+
+        case "W":
+        case "w":
+
+            // Going up
+
+            break;
+
+        case "M":
+        case "m":
+
+            // Open the map
+
+            break;
+
+        default:
+
+            // return error message "Invalid input. TIPS: <WASD for navigation> <M for map>"
+            break;
+
+    }
 
     return 0;
 }
